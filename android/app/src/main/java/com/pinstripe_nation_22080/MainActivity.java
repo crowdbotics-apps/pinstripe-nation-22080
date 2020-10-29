b2d0e54ed32c25e4d6e8c6c506d21165fd6cbc2b
Crowdbotics <team@crowdbotics.com>
2020-10-29 07:46:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pinstripe_nation_22080/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pinstripe_nation_22080;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "pinstripe_nation_22080";
+    }
+}
